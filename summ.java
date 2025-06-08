12df6c902c57c41bc01a2fd4331a6a56801f833a
sathvika <sath@DESKTOP-TD96UI3.localdomain>
2025-06-08 13:23:46 +0000

    fc

---
+++ summ.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumAndAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        double average;
+
+        System.out.println("Enter 10 numbers:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Number " + i + ": ");
+            int num = scanner.nextInt();
+            sum += num;
+        }
+
+        average = sum / 10.0;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+    }
+}
+
